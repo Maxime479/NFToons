90f71df2531b3cd21f60d7e64551bf7f8ab07c52
Maxime479 <45390764+Maxime479@users.noreply.github.com>
2022-11-06 21:29:55 +0100

    Home page init

---
+++ src/js/Home.js
@@ -0,0 +1,24 @@
+
+import '../css/App.css';
+
+export default function Home({...props}) {
+
+    const navigate = props.nav
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+
+
+
+
+
+                <p>
+                    Here we are
+                </p>
+            </header>
+        </div>
+    );
+}
+
