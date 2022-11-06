0a398f917c2b4dc0e3d9f12a38072a186ddf0094
Maxime479 <45390764+Maxime479@users.noreply.github.com>
2022-11-06 21:29:37 +0100

    Router navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
 import logo from './assets/lazy_lion.png';
 import './css/App.css';
+import {Route, Routes, useNavigate} from "react-router-dom";
+import Home from "./js/Home";
 
-function App() {
+
+
+
+
+
+export default function App() {
+
+
+    const navigate = useNavigate();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          NFToons adventure starts here.
-        </p>
-      </header>
-    </div>
+      <div>
+          <div>
+              <Routes>
+                  <Route path="/home" element={<Home nav={navigate} />} />
+                  <Route path="/" element={<Start nav={navigate} />} />
+              </Routes>
+          </div>
+      </div>
   );
 }
 
-export default App;
+
+function Start({...props}) {
+
+    const navigate = props.nav
+
+
+    const navigateToHome = () => {
+        navigate('/home');
+    };
+
+    // const navigateStart = () => {
+    //     navigate('/');
+    // };
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                <img
+                    src={logo}
+                    className="App-logo"
+                    alt="main_logo"
+                    onClick={() => navigateToHome()}
+                />
+
+                <p>
+                    NFToons adventure starts here.
+                </p>
+            </header>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
+import {BrowserRouter as Router} from "react-router-dom";
+import {createRoot} from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+        <App />
+    </Router>
 );
