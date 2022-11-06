cce080f9320a948816e26f57f4e5f57a79408c74
Maxime479 <45390764+Maxime479@users.noreply.github.com>
2022-11-06 22:58:51 +0100

    NFT metadatas

---
+++ src/assets/data/data.js
@@ -0,0 +1,818 @@
+import img01 from "../img/01.jpg";
+import img02 from "../img/02.jpg";
+import img03 from "../img/03.jpg";
+import img04 from "../img/04.jpg";
+import img05 from "../img/05.jpg";
+import img06 from "../img/06.jpg";
+import img07 from "../img/07.jpg";
+import img08 from "../img/08.jpg";
+import img09 from "../img/09.jpg";
+import img10 from "../img/10.jpg";
+import img11 from "../img/11.jpg";
+import img12 from "../img/12.jpg";
+import img13 from "../img/13.jpg";
+import img14 from "../img/14.jpg";
+import img15 from "../img/15.jpg";
+import img16 from "../img/16.jpg";
+import img17 from "../img/17.jpg";
+import img18 from "../img/18.jpg";
+import img19 from "../img/19.jpg";
+import img20 from "../img/20.jpg";
+import img21 from "../img/21.jpg";
+import img22 from "../img/22.jpg";
+import img23 from "../img/23.jpg";
+import img24 from "../img/24.jpg";
+import img25 from "../img/25.jpg";
+import img26 from "../img/26.jpg";
+import img27 from "../img/27.jpg";
+import img28 from "../img/28.jpg";
+import img29 from "../img/29.jpg";
+import img30 from "../img/30.jpg";
+
+
+export const nft_datas = [
+
+    {
+        id: "01",
+        imgUrl: img01,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "cochon"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "584c6233269a83097a7d04d1"
+            }
+        ],
+    },
+
+
+    {
+        id: "02",
+        imgUrl: img02,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "rat"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "03",
+        imgUrl: img03,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "download"
+            },
+            {
+                trait_type: "Head",
+                value: "zebre"
+            },
+            {
+                trait_type: "Eyes",
+                value: "179-1794978_angry-cartoon-eyes-angry-eyes-cartoon-png"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "04",
+        imgUrl: img04,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "ane"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "pTq8o54pc"
+            }
+        ],
+    },
+
+
+    {
+        id: "05",
+        imgUrl: img05,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "belier"
+            },
+            {
+                trait_type: "Eyes",
+                value: "anime-eyes-png-29"
+            },
+            {
+                trait_type: "Mouth",
+                value: "584c6233269a83097a7d04d1"
+            }
+        ],
+    },
+
+
+    {
+        id: "06",
+        imgUrl: img06,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "rat"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "2086833"
+            }
+        ],
+    },
+
+
+    {
+        id: "07",
+        imgUrl: img07,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "zebre"
+            },
+            {
+                trait_type: "Eyes",
+                value: "179-1794978_angry-cartoon-eyes-angry-eyes-cartoon-png"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "08",
+        imgUrl: img08,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "rat"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "pTq8o54pc"
+            }
+        ],
+    },
+
+
+    {
+        id: "09",
+        imgUrl: img09,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "lion"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "584c6233269a83097a7d04d1"
+            }
+        ],
+    },
+
+
+    {
+        id: "10",
+        imgUrl: img10,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "belier"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "11",
+        imgUrl: img11,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "belier"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Cartoon-Eyes-PNG-Transparent-Image"
+            },
+            {
+                trait_type: "Mouth",
+                value: "2086833"
+            }
+        ],
+    },
+
+
+    {
+        id: "12",
+        imgUrl: img12,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "lion"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "584c6233269a83097a7d04d1"
+            }
+        ],
+    },
+
+
+    {
+        id: "13",
+        imgUrl: img13,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "ane"
+            },
+            {
+                trait_type: "Eyes",
+                value: "anime-eyes-png-29"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "14",
+        imgUrl: img14,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "rat"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Cartoon-Eyes-PNG-Transparent-Image"
+            },
+            {
+                trait_type: "Mouth",
+                value: "584c6233269a83097a7d04d1"
+            }
+        ],
+    },
+
+
+    {
+        id: "15",
+        imgUrl: img15,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "rat"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Cartoon-Eyes-PNG-Transparent-Image"
+            },
+            {
+                trait_type: "Mouth",
+                value: "2086833"
+            }
+        ],
+    },
+
+
+    {
+        id: "16",
+        imgUrl: img16,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "ane"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "584c6233269a83097a7d04d1"
+            }
+        ],
+    },
+
+
+    {
+        id: "17",
+        imgUrl: img17,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "ane"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Cartoon-Eyes-PNG-Transparent-Image"
+            },
+            {
+                trait_type: "Mouth",
+                value: "pTq8o54pc"
+            }
+        ],
+    },
+
+
+    {
+        id: "18",
+        imgUrl: img18,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "rat"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "19",
+        imgUrl: img19,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "belier"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Cartoon-Eyes-PNG-Transparent-Image"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "20",
+        imgUrl: img20,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "zebre"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "pTq8o54pc"
+            }
+        ],
+    },
+
+
+    {
+        id: "21",
+        imgUrl: img21,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "belier"
+            },
+            {
+                trait_type: "Eyes",
+                value: "179-1794978_angry-cartoon-eyes-angry-eyes-cartoon-png"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "22",
+        imgUrl: img22,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "cochon"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "23",
+        imgUrl: img23,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "download"
+            },
+            {
+                trait_type: "Head",
+                value: "zebre"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "24",
+        imgUrl: img24,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "cochon"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "25",
+        imgUrl: img25,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "zebre"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "584c6233269a83097a7d04d1"
+            }
+        ],
+    },
+
+
+    {
+        id: "26",
+        imgUrl: img26,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "zebre"
+            },
+            {
+                trait_type: "Eyes",
+                value: "anime-eyes-png-29"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "27",
+        imgUrl: img27,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "download"
+            },
+            {
+                trait_type: "Head",
+                value: "rat"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "28",
+        imgUrl: img28,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "download"
+            },
+            {
+                trait_type: "Head",
+                value: "felin"
+            },
+            {
+                trait_type: "Eyes",
+                value: "179-1794978_angry-cartoon-eyes-angry-eyes-cartoon-png"
+            },
+            {
+                trait_type: "Mouth",
+                value: "584c6233269a83097a7d04d1"
+            }
+        ],
+    },
+
+
+    {
+        id: "29",
+        imgUrl: img29,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "zebre"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Cartoon-Eyes-PNG-Transparent-Image"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+    {
+        id: "30",
+        imgUrl: img30,
+        creator: "Crazy Lion Team",
+        price: 0.013,
+        attributes: [
+            {
+                trait_type: "Background",
+                value: "images"
+            },
+            {
+                trait_type: "Head",
+                value: "ane"
+            },
+            {
+                trait_type: "Eyes",
+                value: "Expression-Cartoon-Eyes-PNG-Transparent-Picture"
+            },
+            {
+                trait_type: "Mouth",
+                value: "22-229670_screaming-mouth-png-bfdi-mouth"
+            }
+        ],
+    },
+
+
+];
+
+
+
