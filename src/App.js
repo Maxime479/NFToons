696d281156fe9469bfcef61c8a888f0e6f582cf1
Maxime479 <45390764+Maxime479@users.noreply.github.com>
2022-11-06 20:25:46 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './assets/lazy_lion.png';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          NFToons adventure starts here.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
