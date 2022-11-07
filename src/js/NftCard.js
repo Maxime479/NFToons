0d9bff71712f9ab80be60055c31f02a5e4759403
Maxime479 <45390764+Maxime479@users.noreply.github.com>
2022-11-07 01:58:22 +0100

    NFT Card component

---
+++ src/js/NftCard.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "../css/NftCard.css";
+import lion_logo from '../assets/lazy_lion.png';
+
+
+const NftCard = (props) => {
+    const { id, title, imgUrl, creator, price, attributes } = props.metadata;
+
+
+    return (
+        <div className="nft_card">
+            <div className="nft_img_container">
+                <img src={imgUrl} alt="" className="nft_img" />
+            </div>
+
+            <div className="nft_content">
+                <h5 className="nft_title">
+                    <Link to={`/market/${id}`}>{title}</Link>
+                </h5>
+
+                <div className="creator_info_container">
+                    <div className="creator_img">
+                        <img src={lion_logo} alt="creator logo"/>
+                    </div>
+
+                    <div className="creator_info">
+                        <div>
+                            <h6>Créé par</h6>
+                            <p>{creator}</p>
+                        </div>
+
+                        <div>
+                            <h6>Prix actuel</h6>
+                            <p>{price} ETH</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="btn_container">
+                    <button
+                        className="buy_btn"
+                    >
+                        Acheter
+                    </button>
+
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NftCard;
