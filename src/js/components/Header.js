b4cbd1ef47f708c82f4f5b2775947996b2a2a0e3
Nicolas <nicolas.lebon00@gmail.com>
2022-11-07 19:33:43 +0100

    Connexion du wallet

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -3,17 +3,18 @@ import React from "react";
 import lion_logo from "../../assets/lazy_lion_base.png";
 import connectWallet from "../functions/connectWallet";
 import '../../css/components/Header.css';
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import Web3 from "web3";
 
 
 export default function Header() {
+
 
     return (
         <header className="Home-header">
 
             <div className="header_left">
-                <img src={lion_logo} alt="lion logo" className="header_image"/>
+                <img src={lion_logo} alt="lion logo" className="header_image" />
 
 
                 <p className="header_title">
@@ -28,6 +29,7 @@ export default function Header() {
             <button
                 onClick={() => connectWallet()}
                 className="connect_wallet_btn"
+                id="bouton de connexion"
             >
                 Connecter le Wallet
             </button>
--- src/js/functions/connectWallet.js
+++ src/js/functions/connectWallet.js
@@ -1,14 +1,32 @@
 
 import React from "react";
+import Web3 from "web3";
 
+export default async function connectWallet() {
 
-export default function connectWallet() {
+        var state = {
+            web3: null,
+            accounts: null,
+            ethBalance: null,
+            networkId: null
+        };
+    
+    console.log("ok");
+    try {
+        const web3 = new Web3(Web3.givenProvider);
+        state.web3 = web3;
+        const accounts = await web3.eth.requestAccounts();
+        console.log(accounts);
+        if(accounts.length > 0) {
+            state.accounts = accounts;
+            state.ethBalance = await web3.eth.getBalance(accounts[0]);
+            state.networkId = await web3.eth.net.getId();
+        }
 
+    } catch (e) {
+        console.log(e);
+    }
 
-
-    return (
-        alert("Connexion du wallet")
-
-    );
+    document.getElementById("bouton de connexion").innerHTML = "Votre solde est de " + Web3.utils.fromWei(state.ethBalance).slice(0,6) + " GETH";
 }
 
