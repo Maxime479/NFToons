43f683c9fec845b1a3a24de013043d825f3fb353
Maxime479 <45390764+Maxime479@users.noreply.github.com>
2022-11-07 01:56:43 +0100

    Home page

--- src/js/Home.js
+++ src/js/Home.js
@@ -1,23 +1,59 @@
 
-import '../css/App.css';
+import '../css/Home.css';
+import {nft_data} from "../assets/data/data.js";
+import React, {useState} from "react";
+import NftCard from "./NftCard";
+import lion_logo from "../assets/lazy_lion_base.png";
+
 
 export default function Home({...props}) {
 
     const navigate = props.nav
 
 
+    const [data, setData] = useState(nft_data);
+
+
     return (
-        <div className="App">
-            <header className="App-header">
+        <div className="Home">
+            <header className="Home-header">
 
-
-
-
-
-                <p>
-                    Here we are
+                <p className="header_title">
+                    NF<span>Toons</span>
                 </p>
             </header>
+
+            <main>
+
+                <div className="upper">
+                    <div className="upper_left">
+
+                        <p className="welcome_message">
+                            Bienvenue sur NF<span>Toons</span> !
+                        </p>
+                        <p className="welcome_message">
+                            Le site de vente de NFTs dédié aux fans de dessins animés (ou pas).
+                        </p>
+
+
+                    </div>
+
+                    <img src={lion_logo} alt="lion logo" className="upper_image"/>
+                </div>
+
+
+                <div className="nft_card_container">
+                    {data?.map((item) => (
+                        <NftCard metadata={item} key={item.id} />
+                    ))}
+                </div>
+            </main>
+
+
+
+
+
+
         </div>
     );
 }
