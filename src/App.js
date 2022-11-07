b1e1f2e8f40b011b41a8fb6cffb490f3be52ac1f
Maxime479 <45390764+Maxime479@users.noreply.github.com>
2022-11-07 16:51:09 +0100

    moved files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './assets/lazy_lion.png';
 import './css/App.css';
 import {Route, Routes, useNavigate} from "react-router-dom";
-import Home from "./js/Home";
+import Home from "./js/pages/Home";
 
 
 
